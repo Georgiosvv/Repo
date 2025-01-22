fb5644f9ec2478557a27a3e5e84b655076931d41
Georgiosvv <gvassiou@iekakmi.edu.gr>
2025-01-22 19:37:41 +0200

    Created main.py that contains directory/UpperString class

---
+++ directory/UpperString.py
@@ -0,0 +1,9 @@
+class UpperString:
+    def __init__(self):
+        self.string = ""
+
+    def get_String(self, string):
+        self.string = string
+
+    def print_String(self):
+        print(self.string.upper())
---
+++ main.py
@@ -0,0 +1,6 @@
+from directory.UpperString import UpperString
+
+if __name__ == "__main__":
+    upper_string = UpperString()
+    upper_string.get_String("hello world")
+    upper_string.print_String()
