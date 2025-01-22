67896a15a6e9e45ba03f5b389cd6e4542d4d46b1
Georgiosvv <gvassiou@iekakmi.edu.gr>
2025-01-22 20:26:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from directory.UpperString import UpperString
 from directory.LowerString import LowerString
 if __name__ == "__main__":
-    text = input("give text")
+    text = input("give text :")
     upper_string = UpperString()
     upper_string.get_String(text)
     upper_string.print_String()
