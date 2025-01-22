7292574693a3b80cda3852156ba1ef14fd6e42b1
E1 Teacher <e1teacher@akmith-edu.local>
2025-01-22 19:51:31 +0200

    my commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from directory.UpperString import UpperString
-
+from directory.LowerString import LowerString
 if __name__ == "__main__":
+    text = input("give text")
     upper_string = UpperString()
-    upper_string.get_String("hello world")
+    upper_string.get_String(text)
     upper_string.print_String()
+    lower_string = LowerString()
+    lower_string.get_String(text)
+    lower_string.print_String()
